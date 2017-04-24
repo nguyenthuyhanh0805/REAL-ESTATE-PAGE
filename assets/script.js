edac97b156893cc4ffd3d95a12e0bc44256bbe22
Hanh Nguyen <nguyenthuyhanh0805@gmail.com>
2017-04-24 23:37:00 +0700

    change menu when change screen width

--- assets/script.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function(){
-    $(".info-detail-item-featured-property").hover(function(){
-        $(this).css("opacity", 1);
-    });
-    $(".info-detail-item-featured-property").mouseleave(function(){
-        $(this).css("opacity", 0);
-    });
-});
