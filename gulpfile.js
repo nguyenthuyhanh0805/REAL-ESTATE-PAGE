a0fe6d2f62d82c91c961b2359b0e2ad6e70f6f36
Hanh Nguyen <nguyenthuyhanh0805@gmail.com>
2017-05-01 16:35:54 +0700

    change style

--- gulpfile.js
+++ gulpfile.js
@@ -6,9 +6,9 @@ var watch = require('gulp-watch');
 var runSequence = require('run-sequence');
 
 var path = {
-scssFiles: ['./assets/*.scss']
+scssFiles: ['./public/*.scss']
 }
-var finalPath = ['./assets/*.css']
+var finalPath = ['./public/*.css']
 gulp.task('clean', function () {
     return gulp.src(finalPath)
         .pipe(clean());
@@ -17,7 +17,7 @@ gulp.task('sass', function () {
     return gulp.src(path.scssFiles)
         .pipe(sass().on('error', sass.logError))
         .pipe(concat('style.css'))
-        .pipe(gulp.dest('./assets'));
+        .pipe(gulp.dest('./public'));
 })
 gulp.task('watch', function () {
     gulp.watch(path.scssFiles, ['sass']);
